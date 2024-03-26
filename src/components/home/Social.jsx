5dfec36dab300e8572e7a1a17fb1933fa148af0d
Hamzshaa <trident32000@gmail.com>
2024-03-26 14:03:19 +0300

    favicon changed

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { FaLinkedin } from "react-icons/fa";
 import { FiGithub } from "react-icons/fi";
-import { TbBrandTelegram } from "react-icons/tb";
+// import { TbBrandTelegram } from "react-icons/tb";
 import { IoLogoWhatsapp } from "react-icons/io5";
 
 import { FaTelegram } from "react-icons/fa";
@@ -13,6 +13,7 @@ function Social() {
         href="https://api.whatsapp.com/send?phone=+251929248080&text=Hello, more information!"
         className="home__social-icon"
         target="_blank"
+        rel="noreferrer"
       >
         <IoLogoWhatsapp />
       </a>
@@ -20,13 +21,15 @@ function Social() {
         href="https://t.me/Hamzshaa"
         className="home__social-icon"
         target="_blank"
+        rel="noreferrer"
       >
         <FaTelegram />
       </a>
       <a
-        href="https://www.linkedin.com/in/hamza-jhad-ba5b36291?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+        href="https://www.linkedin.com/in/hamza-jhad-ba5b36291/"
         className="home__social-icon"
         target="_blank"
+        rel="noreferrer"
       >
         <FaLinkedin />
       </a>
@@ -34,6 +37,7 @@ function Social() {
         href="https://github.com/Hamzshaa/"
         className="home__social-icon"
         target="_blank"
+        rel="noreferrer"
       >
         <FiGithub />
       </a>
