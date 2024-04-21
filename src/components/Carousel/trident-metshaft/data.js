6a8938c2f758b7d1c958157d0c7adecd70e6de34
Hamzshaa <trident32000@gmail.com>
2024-04-21 23:51:31 +0300

    portfolio section images optimised

--- src/components/Carousel/trident-metshaft/data.js
+++ src/components/Carousel/trident-metshaft/data.js
@@ -1,10 +1,10 @@
-import d1 from "./d1.png";
-import d2 from "./d2.png";
-import d3 from "./d3.png";
-import d4 from "./d4.png";
-import d5 from "./d5.png";
-import d6 from "./d6.png";
-import m1 from "./m1.png";
-import m2 from "./m2.png";
+import d1 from "./d1.jpg";
+import d2 from "./d2.jpg";
+import d3 from "./d3.jpg";
+import d4 from "./d4.jpg";
+import d5 from "./d5.jpg";
+import d6 from "./d6.jpg";
+import m1 from "./m1.jpg";
+import m2 from "./m2.jpg";
 
 export const metshaftImg = [d1, d2, d3, d4, d5, d6, m1, m2];
