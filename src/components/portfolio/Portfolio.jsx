00bf09d557a37692679b83f9b30c0badec2c9e1a
Hamzshaa <trident32000@gmail.com>
2024-04-21 21:38:48 +0300

    Portfolio section updated

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,6 +4,7 @@ import "./portfolio.css";
 import img from "../../assets/blog100.jpg";
 import blogLogo from "../../assets/trident_blog.png";
 import cheapDelalaLogo from "../../assets/CheapDelala-logo.png";
+import metshaftLogo from "../../assets/Metshaft.png";
 
 import { IoLogoGithub } from "react-icons/io5";
 import { FiExternalLink } from "react-icons/fi";
@@ -16,8 +17,41 @@ export default function Portfolio() {
       <span className="section__subtitle">my portfolios</span>
       <div className="portfolio__container container grid">
         <div className="portfolio__content">
-          <div className="portfolio__img blog-portfolio">
+          {/* <div className="portfolio__img blog-portfolio">
             <img src={blogLogo} alt="" className="blur zoom" />
+            <div className="portfolio__technologies fade">
+              <div className="portfolio__tools">
+                Technologies Used
+                <li>
+                  <IoCheckboxOutline className="portfolio__check-icon" />
+                  Html/CSS
+                </li>
+                <li>
+                  <IoCheckboxOutline className="portfolio__check-icon" />
+                  React
+                </li>
+                <li>
+                  <IoCheckboxOutline className="portfolio__check-icon" />
+                  Express JS
+                </li>
+                <li>
+                  <IoCheckboxOutline className="portfolio__check-icon" />
+                  MongoDB
+                </li>
+                <li>
+                  <IoCheckboxOutline className="portfolio__check-icon" />
+                  Google Authentication
+                </li>
+                <li>
+                  <IoCheckboxOutline className="portfolio__check-icon" />
+                  and others...
+                </li>
+              </div>
+            </div>
+          </div> */}
+
+          <div className="portfolio__img blog-portfolio">
+            <img src={metshaftLogo} alt="" className="blur zoom" />
             <div className="portfolio__technologies fade">
               <div className="portfolio__tools">
                 Technologies Used
@@ -51,26 +85,27 @@ export default function Portfolio() {
 
           <div className="portfolio__info">
             <div className="">
-              <h3 className="portfolio__title">Blog Website</h3>
+              <h3 className="portfolio__title">Trident Metshaft</h3>
               <h3 className="portfolio__description">
-                a MERN stack blog website with JWT, bcryptjs, react-time-ago,
-                and react-quill. It offers secure user authentication, password
-                hashing, real-time timestamps, and a rich text editor for
-                writing blog posts.
+                Trident Metshaft is a book journey tracker website with progress
+                tracking functionality. It features an attractive and
+                user-friendly admin dashboard for efficient management of book
+                data. The website integrates authentication and authorization,
+                including Google authentication, to ensure secure access for
+                users. Additionally, a notification system has been implemented,
+                allowing administrators to send notifications to users
+                individually or collectively.
               </h3>
             </div>
             <div className="portfolio__buttons">
               <div className="portfolio__github">
-                <a href="https://github.com/Hamzshaa/mern-blog" target="_blank">
+                <a href="https://github.com/Hamzshaa/Metshaft" target="_blank">
                   <IoLogoGithub className="portfolio__button-icon" />
                   Github
                 </a>
               </div>
               <div className="portfolio__online">
-                <a
-                  href="https://trident-mern-blog.onrender.com"
-                  target="_blank"
-                >
+                <a href="https://metshaft.onrender.com/" target="_blank">
                   <FiExternalLink className="portfolio__button-icon " />
                   Online
                 </a>
